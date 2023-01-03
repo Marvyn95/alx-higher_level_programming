f1b06986dab8f86d4f66ca144ceaf7de28112452
Marvyn95 <marvinluyombya@gmail.com>
2023-01-03 14:03:45 -0800

    higher level programming task for alx

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 9):
     p = i + 1
     for j in range(p, 10):
-        print("{}{}".format(i, j), end = '\n' if i == 8 and j == 9 else ', ')
+        print("{}{}".format(i, j), end='\n' if i == 8 and j == 9 else ', ')
