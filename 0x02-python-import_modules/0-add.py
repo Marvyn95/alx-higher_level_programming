218a8efd71803f0ca983ce2ef2542e76d6b4fcd1
Marvyn95 <marvinluyombya@gmail.com>
2023-01-05 10:06:18 -0800

    higher level programming task for alx

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-import add_0
-print(f"a + b = {add_0.add(a, b)}")
+
+if __name__ == "__main__":
+    #print sum of 1 and 2
+    import add_0
+
+    
+    print(f"a + b = {add_0.add(1, 2)}")
