e699133dbfaff21e5acadfd70216591336adc381
Marvyn95 <marvinluyombya@gmail.com>
2023-01-27 03:58:42 -0800

    higher level programming task for alx

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,3 +15,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must bee >= 0")
         self.__size = size
+
+    def area(self):
+        """returns area of the square"""
+        return (self.__size * self.__size)
