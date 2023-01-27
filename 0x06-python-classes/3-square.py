47aa98c7804c522efe2c43f2a7ebb04fc2ee61a2
Marvyn95 <marvinluyombya@gmail.com>
2023-01-27 03:48:31 -0800

    higher level programming task for alx

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class square"""
+
+class Square:
+    """this is a square"""
+
+    def __init__(self, size=0):
+        """initialize new square
+
+        Args:
+        size (int): size of new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must bee >= 0")
+        self.__size = size
