af2d5c3ec41097731f54cbc6f259144eec5cb177
Marvyn95 <marvinluyombya@gmail.com>
2023-01-30 12:33:06 -0800

    higher level programming task for alx

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """defines rectangle"""
 
@@ -48,30 +49,3 @@ class Rectangle:
         """returns area of rec"""
         return (self.__width * self.__height)
 
-    def perimeter(self):
-        """returns perimeter of rec"""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
-
-    def __str__(self):
-        """returns the rec with # printed"""
-        if self.__height == 0 or self.__width == 0:
-            return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
-
-    def __repr__(self):
-        """returns string rep of the rectangle"""
-        rect = "Rectangle({}, {})".format(self.__width, self.__height)
-        return (rect)
-
-    def __del__(self):
-        """prints message when instance is deleted"""
-        type(self).number_of_instances -= 1
-        print("Bye rectangle...")
